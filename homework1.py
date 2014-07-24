a83d998ab87ba4e1685bcfa9c289880e574ffcd5
LTPKevn <kevin05492@Kevins-MacBook-Pro.local>
2014-07-24 12:23:09 -0400

    PFAB
    pfab files and homework

---
+++ homework1.py
@@ -0,0 +1,22 @@
+import math
+
+first = "Kevin"
+last = "Hernandez"
+age = "22"
+
+print first+" "+last+" "+age
+
+sideA = 12.55
+sideB = 17.85
+
+sideC = math.sqrt((sideA**2) +(sideB**2))
+print sideC
+
+operand1 = 95
+operand2 = 64.5
+
+print operand1+operand2
+print operand1-operand2
+print operand1*operand2
+print operand1/operand2
+print operand1%operand2
